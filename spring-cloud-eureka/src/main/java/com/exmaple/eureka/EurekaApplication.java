5ab01f89ac7a0c6331a904aedce22118a3631a78
hongzh <hzh@hongzh.cc>
2018-08-30 18:16:23 +0800

    Initial commit

---
+++ spring-cloud-eureka/src/main/java/com/exmaple/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.exmaple.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 注册中心服务
+ * @author hzh 2018/8/30 15:24
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
