6ad7726704a41af52ddc553d045c980f22755a00
hongzh <hzh@hongzh.cc>
2018-09-01 02:09:23 +0800

    配置中心

--- spring-cloud-config/src/main/java/com/exmaple/config/ConfigApplication.java
+++ spring-cloud-config/src/main/java/com/exmaple/config/ConfigApplication.java
@@ -2,15 +2,15 @@ package com.exmaple.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * SpringCloud 配置中心
  * @author hzh 2018/9/1 上午12:31
  */
 @EnableConfigServer
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigApplication {
 
